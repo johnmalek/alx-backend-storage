080fb03e80e2a153ab02edc0bd2c244acaff423b
Malek <johnmalek54@gmail.com>
2023-04-06 14:03:59 +0000

    Task 1 correction

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
---create table users with id, email, name, country as attributes
---create table if it does not exist
+-- create table users with id, email, name, country as attributes
+-- create table if it does not exist
 CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') DEFAULT 'US'
-)
-
+);
