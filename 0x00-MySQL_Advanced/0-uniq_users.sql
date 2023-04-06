f6df16b94d1099f8d814a4d285124d39b1964c9f
Malek <johnmalek54@gmail.com>
2023-04-06 13:56:25 +0000

    Task 0 correction

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
---create table with id, email, name attributes
---create table if not exist
+-- create table with id, email, name attributes
+-- create table if not exist
 CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
-)
-
+);
