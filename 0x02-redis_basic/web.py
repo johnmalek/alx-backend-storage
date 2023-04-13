e69219f87ccb1b9c70829130d61eed5185b7bf36
Malek <johnmalek54@gmail.com>
2023-04-13 08:19:59 -0700

    Advanced task

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+python function
+"""
+import requests
+import redis
+from functools import wraps
+
+st = redis.Redis()
+
+
+def url_access(method):
+    """
+    decorator to track how many times a
+    particular URL was accessed in the key 'count:{url}'
+    """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = st.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        st.incr(count_key)
+        st.set(cached_key, html)
+        st.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@url_access
+def get_page(url: str) -> str:
+    """
+    Obtain the HTML content of a url and return it
+    """
+    response = requests.get(url)
+    return response.text
